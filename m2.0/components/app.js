f7959c4b8ca098a77ea4d3434bbdc125cde73aec
lh_wang <lh_wang@Ctrip.com>
2016-02-01 23:44:36 +0800

    add compile html

--- m2.0/components/app.js
+++ m2.0/components/app.js
@@ -1,28 +1,31 @@
 
 (function($,win){
   var tpl = $.ajax({url: "./../../components/app.html", async:false});
-  $('body').append(tpl.responseText);
-
-  $('#Header_container').on('click', function(e){
-    console.log('00000--00000');
-  });
-
-  $('#Header_container').hide();
+  var html = template.compile(tpl.responseText);
 
 
-  win.Header = function(opts){
-    var html = template('Header', {name: opts.name});
-    document.getElementById('Header_container').innerHTML = html;
-  };
+  $('body').append(html({name: 'wlh'}));
 
-
-  win.Header.prototype.show = function(){
-    $('#Header_container').show();
-  };
-
-  win.Header.prototype.hide = function(){
-    $('#Header_container').hide();
-  };
+  //$('#Header_container').on('click', function(e){
+  //  console.log('00000--00000');
+  //});
+  //
+  //$('#Header_container').hide();
+  //
+  //
+  //win.Header = function(opts){
+  //  var html = template('Header', {name: opts.name});
+  //  document.getElementById('Header_container').innerHTML = html;
+  //};
+  //
+  //
+  //win.Header.prototype.show = function(){
+  //  $('#Header_container').show();
+  //};
+  //
+  //win.Header.prototype.hide = function(){
+  //  $('#Header_container').hide();
+  //};
 
 
 })($, window);
